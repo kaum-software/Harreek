fb6d657555eace6238297d76b5b838d5aceb500e
Noah Scharrenberg <nscharrenberg@hotmail.com>
2021-06-06 14:38:47 +0200

    add sample test

---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kaum\Harreek\Tests;
+
+use SampleTest;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function basic_test() {
+        $this->assertTrue(true);
+    }
+}
